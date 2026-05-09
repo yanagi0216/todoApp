6efd1afb5bf8449f042c2d8947fde92a5543a812
yanagihara <0216hyanagi@gmail.com>
2026-05-09 12:48:46 +0900

    lesson8 editting

--- db/migrate/20260508042742_create_goals.rb
+++ db/migrate/20260508042742_create_goals.rb
@@ -2,7 +2,7 @@ class CreateGoals < ActiveRecord::Migration[8.1]
   def change
     create_table :goals do |t|
       t.string :title
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true 
 
       t.timestamps
     end
